0a49de2aa859fd97812800ddb7d1bf826939ae85
caenuun93101 <vip3341497@163.com>
2018-03-26 17:47:22 +0800

    完善

--- apps/index/controller/User.php
+++ apps/index/controller/User.php
@@ -1,7 +1,8 @@
 <?php
 namespace app\index\controller;
 use think\Db;
-use think\Request;
+use think\Request; 
+use app\index\model\User as UserModel;
 
 class User
 {
@@ -9,33 +10,63 @@ class User
     {
         //$data = ['name'=>'thinkphp','url'=>'thinkphp.cn'];
 		//return ['code'=>0,'result'=>'error'];
-        echo "1";
+        //echo "2";
+        // $user = new UserModel;
+        // $user->phonenum = '11111';
+        // $user->save();
+        // if ($user->save()) {
+        //     return '用户[ ' . $user->phonenum . ':' . $user->id . ' ]新增成功';
+        // } else {
+        //     return $user->getError();
+        // }
+        // $result = Db::query('select * from zzl_user');
+        // dump($result);
+        $user = UserModel::get(['phonenum'=>'123']);
+        dump($user);
     }
 
     public function getCode(Request $request)
     {
-        $phoneNumber = $request->get('phoneNumber');
-        $g = "/13[123569]{1}\d{8}|15[1235689]\d{8}|188\d{8}/"; 
+        $phoneNumber = $request->get('phoneNum');
+        $g = "/13[12356789]{1}\d{8}|15[1235689]\d{8}|188\d{8}/"; 
         if(preg_match($g,$phoneNumber))
         {
-            return ['code'=>1, 'result'=>'手机号正确'];
+            $user = UserModel::get(['phonenum'=>$phoneNumber]);
+            if ($user == NULL) {
+               return ['code'=>1, 'result'=>'123'];
+            }
+            else
+            {
+                return ['code'=>0, 'result'=>'手机号码已存在'];
+            }
+            
         }else
         {
-            return ['code'=>0, 'result'=>'手机号错误'];
+            return ['code'=>0, 'result'=>'error'];
         }
     }
 
-    public function register(Request $request)
+    public function checkCode(Request $request)
     {
-        //echo($request->get('phone'));
-        $phoneNumber = $request->get('phoneNumber');
         $phoneCode = $request->get('phoneCode');
         if (strcmp($phoneCode, '1234')) {
             return ['code'=>0, 'result'=>'验证码错误'];
         }else
         {
-            return ['code'=>1, 'result'=>'注册成功'];
+            return ['code'=>1, 'result'=>'ok'];
         }
+    }
+
+    public function register(Request $request)
+    {
+        $phoneNumber = $request->get('phoneNum');
+        $password = $request->get('password');
+        $user = new UserModel;
+        $user->phonenum = $phoneNumber;
+        $user->password = $password;
+        $user->username = 'U'.$phoneNumber;
+        $user->save();
+        
     }
 
     public function login(Request $request)
