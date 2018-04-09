ccd41f2d09cbb6118e699de86b5edb2ca60689d1
caenuun93101 <vip3341497@163.com>
2018-04-09 15:58:49 +0800

    no message

--- apps/index/controller/User.php
+++ apps/index/controller/User.php
@@ -8,26 +8,13 @@ class User
 {
     public function index()
     {
-        //$data = ['name'=>'thinkphp','url'=>'thinkphp.cn'];
-		//return ['code'=>0,'result'=>'error'];
-        //echo "2";
-        // $user = new UserModel;
-        // $user->phonenum = '11111';
-        // $user->save();
-        // if ($user->save()) {
-        //     return '用户[ ' . $user->phonenum . ':' . $user->id . ' ]新增成功';
-        // } else {
-        //     return $user->getError();
-        // }
-        // $result = Db::query('select * from zzl_user');
-        // dump($result);
-        $user = UserModel::get(['phonenum'=>'123']);
-        dump($user);
+
+        echo "11111";
     }
 
     public function getCode(Request $request)
     {
-        $phoneNumber = $request->get('phoneNum');
+        $phoneNumber = $request->get('phonenum');
         $g = "/13[12356789]{1}\d{8}|15[1235689]\d{8}|188\d{8}/"; 
         if(preg_match($g,$phoneNumber))
         {
@@ -59,21 +46,87 @@ class User
 
     public function register(Request $request)
     {
-        $phoneNumber = $request->get('phoneNum');
+        $phoneNumber = $request->get('phonenum');
         $password = $request->get('password');
         $user = new UserModel;
         $user->phonenum = $phoneNumber;
         $user->password = $password;
         $user->username = 'U'.$phoneNumber;
-        $user->save();
-        
+        if ($user->save()) {
+             return ['code'=>1, 'result'=>'注册成功'];
+        }else
+        {
+            return ['code'=>0, 'result'=>'注册失败'];
+        }
     }
 
     public function login(Request $request)
     {
-        echo($request->get('name'));
-        
-    	// $data = ['name'=>'crow','sex'=>'男'];
-    	// return ['code'=>1,'result'=>$data];
+        //echo($request->get('phonenum'));
+        $phoneNumber = $request->get('phonenum');
+        $password = $request->get('password');
+        $user = UserModel::get(['phonenum'=>$phoneNumber]);
+        if (strcmp($user->password, $password)) {
+            return ['code'=>0, 'result'=>'手机号或者密码错误'];
+        }else
+        {
+             return ['code'=>1, 'result'=>'登陆成功'];
+        }
+    }
+
+    public function changeUserName(Request $request)
+    {
+        $phoneNumber = $request->get('phonenum');
+        $userName = $request->get('username');
+        $user = UserModel::get(['phonenum'=>$phoneNumber]);
+        $user->username = $userName;
+       if ($user->save()) {
+             return ['code'=>1, 'result'=>'修改成功'];
+        }else
+        {
+            return ['code'=>0, 'result'=>'修改失败'];
+        }
+    }
+
+    public function changePassword(Request $request)
+    {
+        $phoneNumber = $request->get('phonenum');
+        $password = $request->get('password');
+        $user = UserModel::get(['phonenum'=>$phoneNumber]);
+        $user->password = $password;
+       if ($user->save()) {
+             return ['code'=>1, 'result'=>'修改成功'];
+        }else
+        {
+            return ['code'=>0, 'result'=>'修改失败'];
+        }
+    }
+
+    public function changeSex(Request $request)
+    {
+        $phoneNumber = $request->get('phonenum');
+        $sex = $request->get('sex');
+        $user = UserModel::get(['phonenum'=>$phoneNumber]);
+        $user->sex = $sex;
+       if ($user->save()) {
+             return ['code'=>1, 'result'=>'修改成功'];
+        }else
+        {
+            return ['code'=>0, 'result'=>'修改失败'];
+        }
+    }
+
+    public function changeAge(Request $request)
+    {
+        $phoneNumber = $request->get('phonenum');
+        $age = $request->get('age');
+        $user = UserModel::get(['phonenum'=>$phoneNumber]);
+        $user->age = $age;
+       if ($user->save()) {
+             return ['code'=>1, 'result'=>'修改成功'];
+        }else
+        {
+            return ['code'=>0, 'result'=>'修改失败'];
+        }
     }
 }
